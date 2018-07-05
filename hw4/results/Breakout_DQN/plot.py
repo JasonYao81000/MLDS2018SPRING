dc2cf4abddd19a4c968b7128642bc48802badbfd
JasonYao <31233752+JasonYao81000@users.noreply.github.com>
2018-07-05 23:04:37 +0800

    Reward v.s. Episode of DQN on Breakout. (100 > 30)
    Reward v.s. Episode of DQN on Breakout.
    Y-axis: average reward in last 30 episodes

--- hw4/results/Breakout_DQN/plot.py
+++ hw4/results/Breakout_DQN/plot.py
@@ -6,14 +6,83 @@ plt.switch_backend('agg')
 
 if __name__ == "__main__":
 
-    double_dual_rewards = np.load('./hw4_tf_DQN_double_dual/history_recent_avg_reward.npy')
-    double_rewards = np.load('./hw4_tf_DQN_double/history_recent_avg_reward.npy')
-    dual_rewards = np.load('./hw4_tf_DQN_dual/history_recent_avg_reward.npy')
-    none_dqn_rewards = np.load('./hw4_tf_DQN_none/history_recent_avg_reward.npy')
+    # double_dual_rewards = np.load('./hw4_tf_DQN_double_dual/history_recent_avg_reward.npy')
+    # double_rewards = np.load('./hw4_tf_DQN_double/history_recent_avg_reward.npy')
+    # dual_rewards = np.load('./hw4_tf_DQN_dual/history_recent_avg_reward.npy')
+    # none_dqn_rewards = np.load('./hw4_tf_DQN_none/history_recent_avg_reward.npy')
+    recent_episode_num = 30
+
+    double_dual_rewards = []
+    for index, line in enumerate(open('./hw4_tf_DQN_double_dual/log.txt', 'r')):
+        if index == 0 or index == 1:
+            continue
+        episode, step, reward, recent_reward = line.split(',')
+        double_dual_rewards.append(reward)
+
+    double_dual_rewards = np.array(double_dual_rewards)
+    print ('double_dual_rewards.shape[0]: {0}'.format(double_dual_rewards.shape[0]))
+
+    double_dual_recent_avg_reward = []
+    for i in range(double_dual_rewards.shape[0]):
+        recent_rewards = double_dual_rewards[max(0, i - recent_episode_num):i + 1]
+        # Convert string list to float list.
+        recent_rewards = list(map(float, recent_rewards))
+        double_dual_recent_avg_reward.append(sum(recent_rewards) / len(recent_rewards))
+
+    double_rewards = []
+    for index, line in enumerate(open('./hw4_tf_DQN_double/log.txt', 'r')):
+        if index == 0 or index == 1:
+            continue
+        episode, step, reward, recent_reward = line.split(',')
+        double_rewards.append(reward)
+
+    double_rewards = np.array(double_rewards)
+    print ('double_rewards.shape[0]: {0}'.format(double_rewards.shape[0]))
+
+    double_recent_avg_reward = []
+    for i in range(double_rewards.shape[0]):
+        recent_rewards = double_rewards[max(0, i - recent_episode_num):i + 1]
+        # Convert string list to float list.
+        recent_rewards = list(map(float, recent_rewards))
+        double_recent_avg_reward.append(sum(recent_rewards) / len(recent_rewards))
+
+    dual_rewards = []
+    for index, line in enumerate(open('./hw4_tf_DQN_dual/log.txt', 'r')):
+        if index == 0 or index == 1:
+            continue
+        episode, step, reward, recent_reward = line.split(',')
+        dual_rewards.append(reward)
+
+    dual_rewards = np.array(dual_rewards)
+    print ('dual_rewards.shape[0]: {0}'.format(dual_rewards.shape[0]))
+
+    dual_recent_avg_reward = []
+    for i in range(dual_rewards.shape[0]):
+        recent_rewards = dual_rewards[max(0, i - recent_episode_num):i + 1]
+        # Convert string list to float list.
+        recent_rewards = list(map(float, recent_rewards))
+        dual_recent_avg_reward.append(sum(recent_rewards) / len(recent_rewards))
+        
+    none_rewards = []
+    for index, line in enumerate(open('./hw4_tf_DQN_none/log.txt', 'r')):
+        if index == 0 or index == 1:
+            continue
+        episode, step, reward, recent_reward = line.split(',')
+        none_rewards.append(reward)
+
+    none_rewards = np.array(none_rewards)
+    print ('none_rewards.shape[0]: {0}'.format(none_rewards.shape[0]))
+
+    none_recent_avg_reward = []
+    for i in range(none_rewards.shape[0]):
+        recent_rewards = none_rewards[max(0, i - recent_episode_num):i + 1]
+        # Convert string list to float list.
+        recent_rewards = list(map(float, recent_rewards))
+        none_recent_avg_reward.append(sum(recent_rewards) / len(recent_rewards))
 
     plt.clf()
     plt.figure(figsize=(16, 5))
-    plt.plot(double_dual_rewards, label='double dual')
+    plt.plot(double_dual_recent_avg_reward, label='double dual')
     plt.legend(loc='upper left')
     plt.title('Reward v.s. Episode')
     plt.ylabel('Average reward in last 100 episodes')
@@ -22,7 +91,7 @@ if __name__ == "__main__":
 
     plt.clf()
     plt.figure(figsize=(16, 5))
-    plt.plot(double_rewards, label='double')
+    plt.plot(double_recent_avg_reward, label='double')
     plt.legend(loc='upper left')
     plt.title('Reward v.s. Episode')
     plt.ylabel('Average reward in last 100 episodes')
@@ -31,7 +100,7 @@ if __name__ == "__main__":
     
     plt.clf()
     plt.figure(figsize=(16, 5))
-    plt.plot(dual_rewards, label='dual')
+    plt.plot(dual_recent_avg_reward, label='dual')
     plt.legend(loc='upper left')
     plt.title('Reward v.s. Episode')
     plt.ylabel('Average reward in last 100 episodes')
@@ -40,7 +109,7 @@ if __name__ == "__main__":
     
     plt.clf()
     plt.figure(figsize=(16, 5))
-    plt.plot(none_dqn_rewards, label='none')
+    plt.plot(none_recent_avg_reward, label='none')
     plt.legend(loc='upper left')
     plt.title('Reward v.s. Episode')
     plt.ylabel('Average reward in last 100 episodes')
@@ -49,10 +118,10 @@ if __name__ == "__main__":
 
     plt.clf()
     plt.figure(figsize=(16, 5))
-    plt.plot(double_dual_rewards, label='double dual')
-    plt.plot(double_rewards, label='double')
-    plt.plot(dual_rewards, label='dual')
-    plt.plot(none_dqn_rewards, label='none')
+    plt.plot(double_dual_recent_avg_reward, label='double dual')
+    plt.plot(double_recent_avg_reward, label='double')
+    plt.plot(dual_recent_avg_reward, label='dual')
+    plt.plot(none_recent_avg_reward, label='none')
     plt.legend(loc='upper left')
     plt.title('Reward v.s. Episode')
     plt.ylabel('Average reward in last 100 episodes')
